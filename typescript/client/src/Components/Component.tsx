c5e354b59d026c55a72d440fd7643287412fae96
Borges <A447055@santandertecnologia.com.ar>
2023-06-01 12:55:01 -0300

    test

---
+++ typescript/client/src/Components/Component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Component = () => {
+    return (
+        <div
+        // How do I figure out what type aria-posinset expects?
+      aria-posinset={1}
+      // How do I figure out what type onChange expects?
+      onChange={(e) => {}}
+      // How do I get autocomplete with JSX?
+      >
+        </div>
+    )
+}
+
